dda1ee8dc6db2fd30fbeb3bbd577f531d9c2c9bc
Ralph <19510622+RalXYZ@users.noreply.github.com>
2020-11-21 02:02:27 +0800

    button can change score

--- app/src/main/java/xyz/ralxyz/qscmobile/concept/CourseDetailsActivity.kt
+++ app/src/main/java/xyz/ralxyz/qscmobile/concept/CourseDetailsActivity.kt
@@ -2,12 +2,25 @@ package xyz.ralxyz.qscmobile.concept
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class CourseDetailsActivity : AppCompatActivity() {
+    private var score: Double = 2.7
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.course_details)
+        findViewById<TextView>(R.id.score).apply {
+            text = score.toString()
+        }
+    }
+
+    fun subtract(view: View) {
+        score -= 0.3
+        findViewById<TextView>(R.id.score).apply {
+            text = String.format("%.1f", score)
+        }
     }
 
     fun quitActivity(view: View) {
